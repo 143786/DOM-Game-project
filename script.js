289c5ec3cc151ba04354777260998879aca09a72
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-10-24 00:02:08 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -12,6 +12,7 @@
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20; /* state variable => because this score is part of the 
  so-called application state */
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -29,6 +30,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
 
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
 
     // When guess is too high
   } else if (guess > secretNumber) {
@@ -64,5 +70,5 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.guess').value = ' ';
 
   document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.number').style.width = '40rem';
+  document.querySelector('.number').style.width = '15rem';
 });
