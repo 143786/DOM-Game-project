a1f5c348012f531824c56632392aa086ace0cc00
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-10-28 11:50:22 +0100

    Add files via upload

--- Pig-Game-DOM/script.js
+++ Pig-Game-DOM/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+// Selecting elements :
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+/* when we use the
+ getElementById we dont need to put # here so we can use instead 
+of querySelector this one also for id elements  this work exactly the same
+but the get element by ID is supposed to be a little bit faster then
+query selector but maybe that's only relevant if you're selecting like 
+thousands of elements at once. */
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+/* here we are specifying numbers, not strings, but JS will then 
+automatically convert them to strings to display them on the page. */
