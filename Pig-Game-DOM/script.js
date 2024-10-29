84c9aafbf72183f8d2df498ed975a6ad219d62b9
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-10-29 15:16:42 +0100

    Add files via upload

--- Pig-Game-DOM/script.js
+++ Pig-Game-DOM/script.js
@@ -21,27 +21,33 @@ thousands of elements at once. */
 // Starting conditions :
 score0El.textContent = 0;
 score1El.textContent = 0;
+diceEl.classList.add('hidden');
 //
 
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 /* here we are specifying numbers, not strings, but JS will then 
-automatically convert them to strings to display them on the page. */
+automatically convert them to strings to display them on the page.
 
-// here we want to hide the dice  using the technique that we learned in
-// our previeus project: adding  the class in css :
-/* .hidden {
+ here we want to hide the dice  using the technique that we learned in
+ our previeus project: adding  the class in css :
+ .hidden {
   display: none;
-}
 */
-diceEl.classList.add('hidden');
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 // Rolling dice functionality :
 btnRoll.addEventListener('click', function () {
   // 1. Generating a random dice roll
   const dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice);
 
   // 2. Display dice :
   diceEl.classList.remove('hidden');
@@ -55,12 +61,7 @@ btnRoll.addEventListener('click', function () {
       currentScore;
   } else {
     // Switch to next player :
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    switchPlayer();
   }
 });
 
@@ -69,9 +70,20 @@ btnHold.addEventListener('click', function () {
 
   scores[activePlayer] += currentScore;
   // scores [1] = socres [1] + currentScore
-  document.getElementById(`current--${activePlayer}`).textContent =
+  document.getElementById(`score--${activePlayer}`).textContent =
     scores[activePlayer];
   // 2. Check if player's score is >= 100
-  // Finish the game
+  if (scores[activePlayer] >= 20) {
+    // Finish the game
+    document
+      .querySelector(`player--${activePlayer}`)
+      .classList.add('player--winner');
+
+    document
+      .querySelector(`player--${activePlayer}`)
+      .classList.remove('player--winner');
+  }
+
   // Switch to the next player
+  switchPlayer();
 });
